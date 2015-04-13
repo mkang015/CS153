97e7d86ead7d1cd34caa0d4f8e503334977afc38
mkang015 <mkang015@ucr.edu>
2015-04-13 15:01:03 -0700

    Given method: problems exists

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -87,11 +87,13 @@ struct thread
     enum thread_status status;          /* Thread state. */
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
+	int64_t wakeUpTime;					//min add, wake up time for this thread to wake up
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
+	struct list_elem waitelem;			//min add, wait elem container for waitlist
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
